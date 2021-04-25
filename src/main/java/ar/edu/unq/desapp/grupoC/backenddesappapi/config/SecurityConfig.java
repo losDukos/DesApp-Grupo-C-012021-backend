be8211193ea5bdefe12c37b4d50fbc2f67373da5
pache015 <pache.facundo@gmail.com>
2021-04-25 15:58:32 -0300

    Configuration was added in SecurityConfig and findByName was added in UserRepository

--- src/main/java/ar/edu/unq/desapp/grupoC/backenddesappapi/config/SecurityConfig.java
+++ src/main/java/ar/edu/unq/desapp/grupoC/backenddesappapi/config/SecurityConfig.java
@@ -1,15 +1,42 @@
 package ar.edu.unq.desapp.grupoC.backenddesappapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService usDetailService;
+    @Autowired
+    private BCryptPasswordEncoder bcryp;
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+        throws Exception{
+            auth.userDetailsService(usDetailService).passwordEncoder(bcryp);
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 }
--- src/main/java/ar/edu/unq/desapp/grupoC/backenddesappapi/repositories/UserRepository.java
+++ src/main/java/ar/edu/unq/desapp/grupoC/backenddesappapi/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package ar.edu.unq.desapp.grupoC.backenddesappapi.repositories;
 
 import ar.edu.unq.desapp.grupoC.backenddesappapi.model.Title;
+import ar.edu.unq.desapp.grupoC.backenddesappapi.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface UserRepository extends CrudRepository<Title, String> {
 
     List<Title> findAll();
+    User findByName(String name);
 }
