34bf4491e0e8ce18a002726438a39ff811001185
pache015 <pache.facundo@gmail.com>
2021-04-25 15:55:39 -0300

    Create UserService

---
+++ src/main/java/ar/edu/unq/desapp/grupoC/backenddesappapi/services/UserService.java
@@ -0,0 +1,34 @@
+package ar.edu.unq.desapp.grupoC.backenddesappapi.services;
+
+
+import ar.edu.unq.desapp.grupoC.backenddesappapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ar.edu.unq.desapp.grupoC.backenddesappapi.model.User us = repo.findByName(username);
+
+        List<GrantedAuthority> rols = new ArrayList<>();
+        rols.add(new SimpleGrantedAuthority("ADMIN"));
+
+        UserDetails usDetails = new User(us.getName(), us.getPassword(), rols);
+
+        return usDetails;
+    }
+}
