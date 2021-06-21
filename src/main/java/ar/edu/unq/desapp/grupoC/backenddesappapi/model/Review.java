2cdcf9b8288088c1cf006bdc955dbd33bc50916f
Lucas Wojciechow <wojciechowlucas@gmail.com>
2021-06-21 15:19:07 -0300

    first working version of redis pubsub

--- src/main/java/ar/edu/unq/desapp/grupoC/backenddesappapi/model/Review.java
+++ src/main/java/ar/edu/unq/desapp/grupoC/backenddesappapi/model/Review.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Review {
 
     @Id
--- src/main/java/ar/edu/unq/desapp/grupoC/backenddesappapi/services/ReviewService.java
+++ src/main/java/ar/edu/unq/desapp/grupoC/backenddesappapi/services/ReviewService.java
@@ -7,7 +7,11 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.connection.ReactiveSubscription;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -16,6 +20,9 @@ public class ReviewService {
 
     @Autowired
     private ReviewRepository reviewRepository;
+
+    @Autowired
+    private ReactiveRedisOperations<String, Review> redisTemplate;
 
     public List<Review> getReviewsByTitle(String title) {
         return reviewRepository.findAllByReviewedTitleTitleIgnoreCaseContaining(title);
@@ -35,7 +42,10 @@ public class ReviewService {
     public Review getReviewById(Long id) { return reviewRepository.findById(id).get(); }
 
     public Review addReview(Review review) {
-        return reviewRepository.save(review);
+        Review savedReview = reviewRepository.save(review);
+        String topic = getReviewTopic(review.getReviewedTitle().getTitleId());
+        redisTemplate.convertAndSend(topic, savedReview).subscribe();
+        return savedReview;
     }
 
     public Review updateReview(Review review) {
@@ -46,5 +56,23 @@ public class ReviewService {
         Review reviewUpdated = review;
         reviewUpdated.setIsReported(true);
         return this.updateReview(reviewUpdated);
+    }
+
+    public void listenForReviews(String platform, String titleId, String callback) {
+        redisTemplate
+                .listenTo(ChannelTopic.of(getReviewTopic(titleId)))
+                .map(ReactiveSubscription.Message::getMessage)
+                .subscribe((Review review) -> {
+                    System.out.println("Letting subscribers of title "
+                            + review.getReviewedTitle().getTitleId()
+                            + " know that a new review has been written.\nRating: "
+                            + review.getRating());
+                    WebClient.create(callback).post();
+                });
+        System.out.println("Platform " + platform + " subscribed to reviews from title " + titleId);
+    }
+
+    private String getReviewTopic(String titleId) {
+        return "Review for title with ID " + titleId;
     }
 }
