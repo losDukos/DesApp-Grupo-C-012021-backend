44f8b0dd94c7136e571c8480b1b19687aacda7b8
pache0015 <pache.facundo@gmail.com>
2021-06-24 21:33:06 -0300

    fixin test

--- src/test/java/ar/edu/unq/desapp/grupoC/backenddesappapi/integration/ReviewIntegrationTests.java
+++ src/test/java/ar/edu/unq/desapp/grupoC/backenddesappapi/integration/ReviewIntegrationTests.java
@@ -1,6 +1,7 @@
 package ar.edu.unq.desapp.grupoC.backenddesappapi.integration;
 
 import ar.edu.unq.desapp.grupoC.backenddesappapi.builders.ReviewBuilder;
+import ar.edu.unq.desapp.grupoC.backenddesappapi.factory.ControllerTestFactory;
 import ar.edu.unq.desapp.grupoC.backenddesappapi.model.Review;
 import ar.edu.unq.desapp.grupoC.backenddesappapi.model.Title;
 import ar.edu.unq.desapp.grupoC.backenddesappapi.model.User;
@@ -54,17 +55,21 @@ public class ReviewIntegrationTests {
 
     @Test
     void a_title_has_no_reviews() throws Exception {
-        mvc.perform(get("/review/title/" + title.getTitle()))
+        String token = ControllerTestFactory.getUserToken(mvc);
+        mvc.perform(get("/review/title/" + title.getTitle())
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(0)));
     }
 
     @Test
     void a_review_is_retrieved_by_exact_title() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().withPremium(false).build(title);
         reviewRepository.save(review);
 
-        mvc.perform(get("/review/title/" + title.getTitle()))
+        mvc.perform(get("/review/title/" + title.getTitle())
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(review.getId()))));
@@ -72,10 +77,12 @@ public class ReviewIntegrationTests {
 
     @Test
     void a_review_is_retrieved_by_part_of_the_title() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().withPremium(false).build(title);
         reviewRepository.save(review);
 
-        mvc.perform(get("/review/title/" + title.getTitle().substring(0, 2)))
+        mvc.perform(get("/review/title/" + title.getTitle().substring(0, 2))
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(review.getId()))));
@@ -85,6 +92,7 @@ public class ReviewIntegrationTests {
 
     @Test
     void a_review_is_retrieved_by_title_id() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         ReviewBuilder builder = new ReviewBuilder().withPremium(false);
         Title anotherTitle = titleRepository.save(new Title("anotherTitleId", "Another Title"));
         Review anotherReview = builder.build(anotherTitle);
@@ -93,7 +101,8 @@ public class ReviewIntegrationTests {
         review = builder.build(title);
         reviewRepository.save(review);
 
-        mvc.perform(get("/review/id/" + title.getTitleId()))
+        mvc.perform(get("/review/id/" + title.getTitleId())
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(review.getId()))));
@@ -103,10 +112,12 @@ public class ReviewIntegrationTests {
 
     @Test
     void spoilery_reviews_are_retrieved_by_title_id() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().withSpoilerAlert(true).build(title);
         reviewRepository.save(review);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?spoilerAlert=true"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?spoilerAlert=true")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(review.getId()))));
@@ -114,20 +125,24 @@ public class ReviewIntegrationTests {
 
     @Test
     void spoilery_reviews_are_excluded_from_the_search() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().withSpoilerAlert(true).build(title);
         reviewRepository.save(review);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?spoilerAlert=false"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?spoilerAlert=false")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(0)));
     }
 
     @Test
     void argentinian_reviews_are_retrieved_by_title_id() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().withLocation("Argentina").build(title);
         reviewRepository.save(review);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?location=Argentina"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?location=Argentina")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(review.getId()))));
@@ -135,20 +150,24 @@ public class ReviewIntegrationTests {
 
     @Test
     void argentinian_reviews_are_excluded() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().withLocation("Argentina").build(title);
         reviewRepository.save(review);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?location=United States"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?location=United States")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(0)));
     }
 
     @Test
     void english_reviews_are_retrieved_by_title_id() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().withLanguage("English").build(title);
         reviewRepository.save(review);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?language=English"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?language=English")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(review.getId()))));
@@ -156,20 +175,24 @@ public class ReviewIntegrationTests {
 
     @Test
     void english_reviews_are_excluded() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().withLocation("English").build(title);
         reviewRepository.save(review);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?language=Spanish"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?language=Spanish")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(0)));
     }
 
     @Test
     void premium_reviews_are_retrieved_by_title_id() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().withPremium(true).build(title);
         reviewRepository.save(review);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?type=critic"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?type=critic")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(review.getId()))));
@@ -177,10 +200,12 @@ public class ReviewIntegrationTests {
 
     @Test
     void premium_reviews_are_excluded() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().withPremium(true).build(title);
         reviewRepository.save(review);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?type=public"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?type=public")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(0)));
     }
@@ -189,13 +214,15 @@ public class ReviewIntegrationTests {
 
     @Test
     void reviews_are_sorted_by_rating_asc() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         Review lastReview = new ReviewBuilder().withRating(5.0).build(title);
         reviewRepository.save(lastReview);
 
         Review firstReview =  new ReviewBuilder().withRating(3.0).build(title);
         reviewRepository.save(firstReview);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?sort=rating,asc"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?sort=rating,asc")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(firstReview.getId()))))
@@ -204,13 +231,15 @@ public class ReviewIntegrationTests {
 
     @Test
     void reviews_are_sorted_by_rating_desc() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         Review lastReview = new ReviewBuilder().withRating(3.0).build(title);
         reviewRepository.save(lastReview);
 
         Review firstReview =  new ReviewBuilder().withRating(5.0).build(title);
         reviewRepository.save(firstReview);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?sort=rating,desc"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?sort=rating,desc")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(firstReview.getId()))))
@@ -219,13 +248,15 @@ public class ReviewIntegrationTests {
 
     @Test
     void reviews_are_sorted_by_date_asc() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         Review lastReview = new ReviewBuilder().withDate(new SimpleDateFormat("dd-MM-yyyy").parse("06-06-2022")).build(title);
         reviewRepository.save(lastReview);
 
         Review firstReview =  new ReviewBuilder().withDate(new SimpleDateFormat("dd-MM-yyyy").parse("06-06-2020")).build(title);
         reviewRepository.save(firstReview);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?sort=date,asc"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?sort=date,asc")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(firstReview.getId()))))
@@ -234,13 +265,15 @@ public class ReviewIntegrationTests {
 
     @Test
     void reviews_are_sorted_by_date_desc() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         Review lastReview = new ReviewBuilder().withDate(new SimpleDateFormat("dd-MM-yyyy").parse("06-06-2020")).build(title);
         reviewRepository.save(lastReview);
 
         Review firstReview =  new ReviewBuilder().withDate(new SimpleDateFormat("dd-MM-yyyy").parse("06-06-2022")).build(title);
         reviewRepository.save(firstReview);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?sort=date,desc"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?sort=date,desc")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(firstReview.getId()))))
@@ -251,23 +284,27 @@ public class ReviewIntegrationTests {
 
     @Test
     void reviews_are_paginated() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         Review firstReview = new ReviewBuilder().build(title);
         reviewRepository.save(firstReview);
 
         Review lastReview =  new ReviewBuilder().build(title);
         reviewRepository.save(lastReview);
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?page=0&size=1"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?page=0&size=1")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(firstReview.getId()))));
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?page=1&size=1"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?page=1&size=1")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].id", comparesEqualTo(Math.toIntExact(lastReview.getId()))));
 
-        mvc.perform(get("/review/id/" + title.getTitleId() + "?page=0&size=2"))
+        mvc.perform(get("/review/id/" + title.getTitleId() + "?page=0&size=2")
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)));
     }
@@ -276,8 +313,10 @@ public class ReviewIntegrationTests {
 
     @Test
     void a_public_review_is_created() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         String json = "{\"reviewedTitle\": {\"titleId\": \"" + title.getTitleId() + "\"}}";
-        mvc.perform(post("/review").contentType(MediaType.APPLICATION_JSON).content(json))
+        mvc.perform(post("/review").contentType(MediaType.APPLICATION_JSON).content(json)
+                .header("Authorization", "Bearer " + token))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.reviewedTitle.titleId", equalTo(title.getTitleId())))
         .andExpect(jsonPath("$.premium", is(false)));
@@ -285,8 +324,10 @@ public class ReviewIntegrationTests {
 
     @Test
     void a_premium_review_is_created() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         String json = "{\"premium\": true, \"reviewedTitle\": {\"titleId\": \"" + title.getTitleId() + "\"}}";
-        mvc.perform(post("/review").contentType(MediaType.APPLICATION_JSON).content(json))
+        mvc.perform(post("/review").contentType(MediaType.APPLICATION_JSON).content(json)
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.reviewedTitle.titleId", equalTo(title.getTitleId())))
                 .andExpect(jsonPath("$.premium", is(true)));
@@ -296,12 +337,14 @@ public class ReviewIntegrationTests {
 
     @Test
     void a_review_is_rated_possitively() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().build(title);
         Review savedReview = reviewRepository.save(review);
         User savedUser = userRepository.save(new User());
 
         String json = "{\"positive\": true, \"userId\": " + savedUser.getId() + "}";
-        mvc.perform(post("/review/" + savedReview.getId()).contentType(MediaType.APPLICATION_JSON).content(json))
+        mvc.perform(post("/review/" + savedReview.getId()).contentType(MediaType.APPLICATION_JSON).content(json)
+                .header("Authorization", "Bearer " + token))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.user.id", equalTo(savedUser.getId().intValue())))
         .andExpect(jsonPath("$.positive", is(true)));
@@ -309,12 +352,14 @@ public class ReviewIntegrationTests {
 
     @Test
     void a_review_is_rated_negatively() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().build(title);
         Review savedReview = reviewRepository.save(review);
         User savedUser = userRepository.save(new User());
 
         String json = "{\"positive\": false, \"userId\": " + savedUser.getId() + "}";
-        mvc.perform(post("/review/" + savedReview.getId()).contentType(MediaType.APPLICATION_JSON).content(json))
+        mvc.perform(post("/review/" + savedReview.getId()).contentType(MediaType.APPLICATION_JSON).content(json)
+                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.user.id", equalTo(savedUser.getId().intValue())))
                 .andExpect(jsonPath("$.positive", is(false)));
@@ -322,15 +367,18 @@ public class ReviewIntegrationTests {
 
     @Test
     void a_review_is_edited_and_the_amount_of_rates_stays_the_same() throws Exception {
+        String token = ControllerTestFactory.getUserToken(mvc);
         review = new ReviewBuilder().build(title);
         Review savedReview = reviewRepository.save(review);
         User savedUser = userRepository.save(new User());
 
         String negativeJson = "{\"positive\": false, \"userId\": " + savedUser.getId() + "}";
-        mvc.perform(post("/review/" + savedReview.getId()).contentType(MediaType.APPLICATION_JSON).content(negativeJson));
+        mvc.perform(post("/review/" + savedReview.getId()).contentType(MediaType.APPLICATION_JSON).content(negativeJson)
+                .header("Authorization", "Bearer " + token));
 
         String positiveJson = "{\"positive\": true, \"userId\": " + savedUser.getId() + "}";
-        mvc.perform(post("/review/" + savedReview.getId()).contentType(MediaType.APPLICATION_JSON).content(positiveJson));
+        mvc.perform(post("/review/" + savedReview.getId()).contentType(MediaType.APPLICATION_JSON).content(positiveJson)
+                .header("Authorization", "Bearer " + token));
 
         Review retrievedReview = reviewRepository.findById(savedReview.getId()).get();
 
